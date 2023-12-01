1e0c04b9299d920242235b74bb862546472ed738
Nkuria <nkuria01@gmail.com>
2023-12-01 23:39:01 +0300

    Tried out various concepts

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia perferendis et eaque accusantium, dicta illum nihil laudantium, suscipit earum quo, quod beatae! Nihil sapiente iste accusantium neque voluptate consequuntur facere alias eos soluta officia nobis quibusdam est, aperiam ducimus? Error similique molestias quisquam cupiditate fuga sed ipsam ducimus voluptates harum.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px
+}
+
+p {
+  margin: 20px 0;
+}
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad debitis aliquam non veniam quos beatae praesentium ducimus laboriosam sequi sed soluta, assumenda iste a libero explicabo rerum vel optio exercitationem! Nam ipsum aliquid vero dolor recusandae illo, voluptatem incidunt quas, ducimus et quos exercitationem iusto laboriosam, aperiam deleniti voluptate veniam.</p>
+  </div>
+</template>
+
+<script setup>
+
+definePageMeta({
+  layout: 'products'
+})
+
+const { id } = useRoute().params
+
+</script>
+
+<style scoped>
+
+</style>
